f6af772921933a8ced3707932f1d4114aff7b5e6
Nikhil Raj <nikhil4rajj@gmail.com>
2026-01-05 04:05:23 +0530

    added: feature and discover section

---
+++ client/src/app/(nondashboard)/landing/FeaturesSection.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from 'react'
+import { motion } from "framer-motion"
+import Image from 'next/image';
+import Link from 'next/link';
+
+const containerVariants= {
+    hidden: { opacity: 0, y: 50},
+    visible: {
+        opacity:1,
+        y:0,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.2
+        }
+    }
+};
+
+const itemVariants = {
+    hidden: { opacity:0 , y: 20 },
+    visible: { opacity:1 , y:0},
+};
+
+const FeaturesSection = () => {
+  return (
+    <motion.div
+    initial= "hidden"
+    whileInView= "visible"
+    viewport= {{ once: true}}
+    variants= {containerVariants}
+    className="py-24 px-6 sm:px-8 lg:px-12 xl:px-16 bg-white"
+    >
+        <div className="max-w-4xl xl:max-w-6xl mx-auto">
+            <motion.h2
+                variants={itemVariants}
+                className="text-3xl font-bold text-center mb-12 w-full sm:w-2/3 mx-auto"
+            >
+                Quickly find the home that you can find using our effective search filter!
+            </motion.h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16">
+                {[0,1,2].map((Index) => (
+                    <motion.div key={Index} variants={itemVariants}>
+                        <FeatureCard 
+                            imageSrc={`/landing-search${ 3 - Index}.png`}
+                            title={
+                            [
+                                "Trustworthy and Verified Listings",
+                                "Browse Rental Listings with Ease",
+                                "Simplify Your Rental Search with Advanced",
+                            ][Index]
+                            }
+                            description={
+                                [
+                                    "Discover the best rental options with user reviews and ratings.",
+                                    "Get access to user reviews and ratings for a better understanding of rental options.",
+                                    "Find trustworthy and verified rental listings to ensure a hassle-free experience.",
+                                ][Index]
+                            }
+                            linkText={["Explore", "Search", "Discover"][Index]}
+                            linkHref={["/explore", "/search", "/discover"][Index]}
+                        />
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+    </motion.div>
+  )
+};
+
+const FeatureCard= ({
+    imageSrc,
+    title,
+    description,
+    linkText,
+    linkHref,
+}: {
+    imageSrc: string;
+    title: string; 
+    description: string;
+    linkText: string;
+    linkHref: string;
+}) => (
+    <div className="text-center">
+        <div className="p-4 rounded-lg mb-4 flex items-center justify-center h-48">
+            <Image
+                src={imageSrc}
+                width={400}
+                height={400}
+                className="w-full h-full object-contain"
+                alt={title}
+            />
+        </div>
+        <h3 className="">{title}</h3>
+        <p className=""> {description} </p>
+        <Link
+            href={linkHref}
+            className="inline-block border border-gray-300 rounded px-4 py-2 hover:bg-gray-100"
+            scroll={false}
+        >
+            {linkText}
+        </Link>
+    </div>
+)
+
+export default FeaturesSection
--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import CallToAction from './CallToAction'
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToAction />
     </div>
   )
 }
