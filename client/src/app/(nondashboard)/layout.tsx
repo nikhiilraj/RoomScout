ce0b8ca849b873e139f7b8540cedb4e52b3a80b1
Nikhil Raj <nikhil4rajj@gmail.com>
2026-01-04 20:01:52 +0530

    add: navbar fix: global.css

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,12 +1,11 @@
-import React, { Children } from 'react'
+import Navbar from '@/components/Navbar'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div>
-        {/* <Navbar /> */}
-        navbar
-        <main className={`h-full flex w-full flex-col pt-[$NAVBAR_HEIGHT]`}>
+        <Navbar />
+        <main className={`h-full flex w-full flex-col pt-[${NAVBAR_HEIGHT}]`}>
             {children}
         </main>
     </div>
